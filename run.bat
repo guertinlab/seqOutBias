b264161eee254464a1948a7cad98d55028760b63
Andre L. Martins <alm253@cornell.edu>
2017-01-19 08:52:50 +0000

    Remove unnecessary files

--- run.bat
+++
@@ -1,1 +0,0 @@
-cargo run --release -- C:/Users/spark/Documents/tmp/fasta/Drosophila_melanogaster.BDGP6.dna.chromosome.2L.fa %1
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cargo run --release -- ../Drosophila_melanogaster.BDGP6.dna.chromosome.2L.fa $1
